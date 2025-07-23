39db9b3e9b1ea212ae530b477ce3dd99c4d52ab2
Raşit <22110207017@ogrenci.bartin.edu.tr>
2025-07-23 13:56:00 +0300

    Validation Kuralları İyileştirildi

---
+++ HTYS.DataAccessLayer/Migrations/20250723084631_iyilestirme.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HTYS.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class iyilestirme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Semtler");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DogumYeri",
+                table: "Musteriler",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DogumYeri",
+                table: "Musteriler",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.CreateTable(
+                name: "Semtler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IlceId = table.Column<int>(type: "int", nullable: false),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Semtler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Semtler_Ilceler_IlceId",
+                        column: x => x.IlceId,
+                        principalTable: "Ilceler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Semtler_IlceId",
+                table: "Semtler",
+                column: "IlceId");
+        }
+    }
+}
